e97e0bf41b1720a6270f93542e9b6fb9b395ae9e
darcykekw <darcyclerigo25@gmail.com>
2025-10-31 10:01:01 +0800

    enhance admin interface

--- projectsite/paymentorg/admin.py
+++ projectsite/paymentorg/admin.py
@@ -3,18 +3,61 @@ from django.utils.html import format_html
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
-from .models import Student, Officer,Organization, FeeType, PaymentRequest, Payment, Receipt, ActivityLog,AcademicYearConfig
+from .models import Student, Officer, Organization, FeeType, PaymentRequest, Payment, Receipt, ActivityLog, AcademicYearConfig
 
-admin.site.register(Student)
-admin.site.register(Officer)
-admin.site.register(Organization)
-admin.site.register(FeeType)
-admin.site.register(PaymentRequest)
-admin.site.register(Payment)
-admin.site.register(Receipt)
-admin.site.register(ActivityLog)
-admin.site.register(AcademicYearConfig)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['student_id_number', 'get_full_name', 'course', 'year_level', 'college', 'email']
+    list_filter = ['college', 'course', 'year_level', 'academic_year', 'semester']
+    search_fields = ['student_id_number', 'first_name', 'last_name', 'email']
+    readonly_fields = ['created_at', 'updated_at']
 
+@admin.register(Officer)
+class OfficerAdmin(admin.ModelAdmin):
+    list_display = ['employee_id', 'get_full_name', 'organization', 'role', 'email']
+    list_filter = ['organization', 'role']
+    search_fields = ['employee_id', 'first_name', 'last_name', 'email']
 
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'department', 'contact_email', 'booth_location']
+    search_fields = ['name', 'code', 'department']
 
-#Register your models here
+@admin.register(FeeType)
+class FeeTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'organization', 'amount', 'academic_year', 'semester', 'deadline']
+    list_filter = ['organization', 'academic_year', 'semester']
+    search_fields = ['name', 'organization__name']
+
+@admin.register(PaymentRequest)
+class PaymentRequestAdmin(admin.ModelAdmin):
+    list_display = ['queue_number', 'student', 'organization', 'amount', 'status', 'created_at']
+    list_filter = ['status', 'organization', 'created_at']
+    search_fields = ['queue_number', 'student__student_id_number']
+    readonly_fields = ['request_id', 'created_at', 'updated_at']
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['or_number', 'student', 'organization', 'amount', 'status', 'created_at']
+    list_filter = ['status', 'organization', 'payment_method']
+    search_fields = ['or_number', 'student__student_id_number']
+    readonly_fields = ['created_at', 'updated_at']
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ['or_number', 'payment', 'email_sent', 'sms_sent', 'created_at']
+    list_filter = ['email_sent', 'sms_sent']
+    search_fields = ['or_number']
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'action', 'created_at']
+    list_filter = ['action', 'created_at']
+    search_fields = ['user__username', 'action']
+    readonly_fields = ['created_at']
+
+@admin.register(AcademicYearConfig)
+class AcademicYearConfigAdmin(admin.ModelAdmin):
+    list_display = ['academic_year', 'semester', 'start_date', 'end_date', 'is_current']
+    list_filter = ['is_current']
+    search_fields = ['academic_year']
