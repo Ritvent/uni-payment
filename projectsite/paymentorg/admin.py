14270480e60c3cc5e127ebc2fbd211a37888a2a5
mosshead <garcianodianarakristy@gmail.com>
2025-10-31 08:06:47 +0800

    added the rest of the models and initially registered

--- projectsite/paymentorg/admin.py
+++ projectsite/paymentorg/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Student, Officer,Organization, FeeType, PaymentRequest, Payment, Receipt, ActivityLog,AcademicYearConfig
+
+admin.site.register(Student)
+admin.site.register(Officer)
+admin.site.register(Organization)
+admin.site.register(FeeType)
+admin.site.register(PaymentRequest)
+admin.site.register(Payment)
+admin.site.register(Receipt)
+admin.site.register(ActivityLog)
+admin.site.register(AcademicYearConfig)
+
+
+
+#Register your models here
